19e0c22716fa0e8c3b2c784cee193fa64b29b495
bamboo <avikbascra@gmail.com>
2014-04-26 21:43:27 +0300

    add item task post

--- Anywall/src/com/parse/anywall/model/AnywallPost.java
+++ Anywall/src/com/parse/anywall/model/AnywallPost.java
@@ -1,41 +1,46 @@
 package com.parse.anywall.model;
 
-import com.parse.ParseClassName;
-import com.parse.ParseGeoPoint;
-import com.parse.ParseObject;
-import com.parse.ParseQuery;
-import com.parse.ParseUser;
+import com.parse.*;
 
 /**
  * Data model for a post.
  */
 @ParseClassName("Posts")
 public class AnywallPost extends ParseObject {
-  public String getText() {
-    return getString("text");
-  }
 
-  public void setText(String value) {
-    put("text", value);
-  }
+    public String getText() {
+        return getString("text");
+    }
 
-  public ParseUser getUser() {
-    return getParseUser("user");
-  }
+    public void setText(String value) {
+        put("text", value);
+    }
 
-  public void setUser(ParseUser value) {
-    put("user", value);
-  }
+    public ParseUser getUser() {
+        return getParseUser("user");
+    }
 
-  public ParseGeoPoint getLocation() {
-    return getParseGeoPoint("location");
-  }
+    public void setDescription(String desc) {
+        put("desc", desc);
+    }
 
-  public void setLocation(ParseGeoPoint value) {
-    put("location", value);
-  }
+    public String getDescription() {
+        return getString("desc");
+    }
 
-  public static ParseQuery<AnywallPost> getQuery() {
-    return ParseQuery.getQuery(AnywallPost.class);
-  }
+    public void setUser(ParseUser value) {
+        put("user", value);
+    }
+
+    public ParseGeoPoint getLocation() {
+        return getParseGeoPoint("location");
+    }
+
+    public void setLocation(ParseGeoPoint value) {
+        put("location", value);
+    }
+
+    public static ParseQuery<AnywallPost> getQuery() {
+        return ParseQuery.getQuery(AnywallPost.class);
+    }
 }
